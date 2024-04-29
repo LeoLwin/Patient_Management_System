7e4896365d80ef8891f47ca05c94472d01e129a5
KHL <phoekaung.3819@gmail.com>
2024-04-29 10:57:23 +0630

    mysql update

--- patient_management_system.sql
+++ patient_management_system.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `patient_management_system`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `patient_management_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `patient_management_system`;
+
+--
 -- Table structure for table `files`
 --
 
@@ -215,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-29 10:45:36
+-- Dump completed on 2024-04-29 10:56:56
